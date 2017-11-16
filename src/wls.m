c59d79bfc7df1f1526b0fa4188b3b4b21eaf725c
reyesc <celso.reyes@sed.ethz.ch>
2017-11-16 17:41:28 +0100

    sped up wls (to 2x), got rid of global S

--- src/wls.m
+++ src/wls.m
@@ -1,44 +1,52 @@
-function [A,B,err] = wls(x,y)
+function [A,B, S, err] = wls(x,y)
     %WEIGHTED LINEAR LEAST SQUARES REGRESSION
-    %	WLS(x,y) finds the A and B coefficients of A log cumalative frequency
+    %	[A, B, S, err] = WLS(x,y) finds the A and B coefficients of A log cumalative frequency
     %curve and the error.
-    
+    %      A, B: a- and b- values of a weighted linear regression fit.
+    %    err: estimate of the std deviation of the error in predicting a future observation at X by A and B
+    %      S: contains fields for triangular factor(R) from QR decomp... see polyfit (used in polyval)
+    %    
     %report_this_filefun(mfilename('fullpath'));
+    %partially vectorized version
     
-    global S % output of POLYFIT used for error estimates
-    mima = min(x);
+%    global S % output of POLYFIT used for error estimates
+    %mima = min(x);
     if any(size(x) ~= size(y))
         error('X and Y vectors must be the same size.')
     end
     x = x(:);
     y = y(:);
-    l = isinf(y); y(l) = [];x(l) = [];
+    l = isinf(y); 
+    y(l) = [];
+    x(l) = [];
     % weight the values
-    wx = ones(1,ceil(sum(10.^(y))));  wy = wx; k=1;
+    teny= 10.^y;
+    wx = ones(1,ceil(sum(teny)));  
+    wy = wx; 
+    fteny=floor(teny);
+    ks = cumsum([1; fteny]);
     for i = 1:length(x)
-        wx(k:floor(10.^(y(i))+k-1)) = wx(k:floor(10.^(y(i))+k-1))*x(i);
-        wy(k:floor(10.^(y(i))+k-1)) = wy(k:floor(10.^(y(i))+k-1))*(10.^y(i));
-        k = floor(10.^(y(i)) + k);
+        wx(ks(i):ks(i+1)-1) = wx(ks(i):ks(i+1)-1) * x(i);
+        wy(ks(i):ks(i+1)-1) = wy(ks(i):ks(i+1)-1) * teny(i);
     end
-    x = wx;
-    y = log10(wy);
+    %x = wx;
+    %y = log10(wy);
     
-    clear wx
-    clear wy
-    l = x  > mima;
+    l = wx  > min(x);%mima;
     
     %[B, A,err ] = ma(x',y');
     %b2 = -abs(B);
     
-    if length(x(l)) > 5
-        [p,S] = polyfit(x(l),y(l),1);
+    if sum(l) > 5
+        [p,S] = polyfit(wx(l),log10(wy(l)),1);
     else
         p = [NaN NaN] ;
     end
     A = p(2);
     B = p(1) ;
-    
-    [~,err] = polyval(p,x,S);
-    err = mean(err);
+    if nargout==4
+        [~,err] = polyval(p,wx,S);
+        err = mean(err);
+    end
 end
 
