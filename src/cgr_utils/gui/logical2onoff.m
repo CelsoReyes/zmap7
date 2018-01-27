694005a18ec588cdc8df483dc30a0d41cbd6d699
reyesc <celso.reyes@sed.ethz.ch>
2018-01-27 22:21:11 +0100

    resolved name conflict with an imaging toolbox

--- src/cgr_utils/gui/logical2onoff.m
+++
@@ -1,8 +0,0 @@
-function out = logical2onoff(val)
-    % logical2onoff returns 'on' if true, 'off' if false
-    if val
-        out = 'on';
-    else
-        out = 'off';
-    end
-end
---
+++ src/cgr_utils/gui/tf2onoff.m
@@ -0,0 +1,8 @@
+function out = tf2onoff(val)
+    % TF2ONOFF returns 'on' if true, 'off' if false
+    if val
+        out = 'on';
+    else
+        out = 'off';
+    end
+end
