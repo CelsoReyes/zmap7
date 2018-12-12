71d69a139511750a476c80517eaa1405434021d0
Celso Reyes <celsoborealis@gmail.com>
2018-12-12 17:05:32 +0100

    with basic progress monitor (that will change)

--- sample_low_level_http_request.m
+++ sample_low_level_http_request.m
@@ -4,9 +4,9 @@
 % with the anticipation that matlab.net.http.ProgressMonitor
 % will be used to provide feedback.
 
-% U = matlab.net.URI('http://service.iris.edu/fdsnws/event/1/count?starttime=2018-01-11T00:00:00&orderby=time&format=text&nodata=404');
+% U = matlab.net.URI('http://service.iris.edu/fdsnws/event/1/query?starttime=2018-01-11T00:00:00&orderby=time&format=text&nodata=404');
 tic
-U = matlab.net.URI('http://service.iris.edu/fdsnws/event/1/query?starttime=2018-01-11T00:00:00&orderby=time&format=text&nodata=404');
+U = matlab.net.URI('http://service.iris.edu/fdsnws/event/1/query?starttime=2018-09-11T00:00:00&orderby=time&format=text&nodata=404');
 method = matlab.net.http.RequestMethod.GET;
 type1 = matlab.net.http.MediaType('text/*');
 acceptField = matlab.net.http.field.AcceptField([type1]);
@@ -16,14 +16,16 @@ request = matlab.net.http.RequestMessage(method,header);
 
 consumer=matlab.net.http.io.StringConsumer;
 
-[resp,req,hist] = request.send(U,matlab.net.http.HTTPOptions('SavePayload',true),consumer);
+[resp,req,hist] = request.send(U,matlab.net.http.HTTPOptions('SavePayload',true,'ProgressMonitorFcn',@MyProgressMonitorNew,'UseProgressMonitor',true),consumer);
 % show(request)
 % resp.show
-ss=strsplit(string(char(resp.Body.Payload')),newline)';
+
+% if there is an error, it would be shown in hist.Response.Body.Data
+ss=strsplit(string(char(resp.Body.Data')),newline)';
 numel(ss)
 %%
 f=fopen('junkk.dat','w');
-fprintf(f,"%s",char(resp.Body.Payload'));
+fprintf(f,"%s",resp.Body.Data); %resp.Body.Payload
 fclose(f);
 %%
 ZG.primeCatalog = import_fdsn_event(1,'junk.dat')
